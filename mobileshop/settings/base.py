fe368fd2590efb1f6825c59798a99fd1413734f6
navin <navinprasandh@gmail.com>
2021-04-17 01:07:36 +0530

    settings

--- mobileshop/settings/base.py
+++ mobileshop/settings/base.py
@@ -4,7 +4,7 @@ from decouple import config
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'kobl@t=yw9d*0y%jt2gjnq78=u!z_rrxb&w8e47l!(jz@m79zy'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
